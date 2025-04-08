5fc419a81a8eb320a9aed349513f7d20227d613a
lauramartiarribas <157625065+lauramartiarribas@users.noreply.github.com>
2025-04-08 16:09:54 +0200

    8 de abril

--- src/main/java/com/example/pecl_pcd_final/HelloController.java
+++ src/main/java/com/example/pecl_pcd_final/HelloController.java
@@ -1,14 +1,38 @@
 package com.example.pecl_pcd_final;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController  {
     @FXML
-    private Label welcomeText;
+    private Button startButton;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    void onStartButtonClick(ActionEvent event) {{
+        Stage stageBorrar= (Stage) startButton.getScene().getWindow();
+        stageBorrar.close();
+        Stage stage = new Stage();
+        stage.show();
+        FXMLLoader fxmlLoader=new FXMLLoader(HelloApplication.class.getResource("com/example/pecl_pcd_final/ProgramaP.fxml"));
+        try {
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setTitle("Programa");
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+        } catch (Exception e) { e.printStackTrace();
+        }
+
     }
-}
+
+
+}}
